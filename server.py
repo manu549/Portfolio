477c594ff6d7c3cef4d6279a9109077d2b0077b6
manu549 <manujalli1234@gmail.com>
2020-04-10 19:46:41 +0530

    done

---
+++ server.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request, redirect
+import csv
+app = Flask(__name__)  # Flask class uses to instantiate an app
+
+
+@app.route('/')
+# anytime we hit a slash/route,function hello_world is defined and returned Hello, World!'
+def home():
+    return render_template('index.html')
+
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+
+
+def write_to_file(data):
+    with open(file='./database.txt', mode='a') as database:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        database.write(f'\n{email},{subject},{message}')
+
+
+def write_to_csv(data):
+    with open(file='./database.csv', mode='a') as database2:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return redirect('ThankYou.html')
+        except:
+            return 'did not save to database'
+    else:
+        return 'something went wrong.Try again!'
