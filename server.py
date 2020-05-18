e2481bbe112585ca58fc1c43c93971230618e7e6
manu549 <manujalli1234@gmail.com>
2020-05-18 17:42:46 +0530

    my portfolio website

--- server.py
+++ server.py
@@ -1,5 +1,10 @@
 from flask import Flask, render_template, request, redirect
 import csv
+from translate import Translator
+import requests
+import hashlib
+from PIL import Image, ImageFilter
+
 app = Flask(__name__)  # Flask class uses to instantiate an app
 
 
@@ -42,3 +47,54 @@ def submit_form():
             return 'did not save to database'
     else:
         return 'something went wrong.Try again!'
+
+
+@app.route('/translate', methods=['POST', 'GET'])
+def translate():
+    if request.method == 'POST':
+        text = request.form['text']
+        language = request.form['language']
+        translator = Translator(to_lang=language)
+        translation = translator.translate(text)
+        return render_template("translator.html", result=translation)
+
+
+def request_api_data(query_char):
+    url = 'https://api.pwnedpasswords.com/range/' + query_char
+    res = requests.get(url)
+    if res.status_code != 200:
+        raise RuntimeError(f'Error fetching:{res.status_code}, check the api and try again')
+    return res
+
+
+def get_password_leaks_count(hashes, hash_to_check):
+    hashes = (line.split(':') for line in hashes.text.splitlines())
+    for h, count in hashes:
+        if h == hash_to_check:
+            return count
+    return 0
+
+
+def pwned_api_check(password):
+    # check password if it exists in api response
+    sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    first_five, tail = sha1password[:5], sha1password[5:]
+    response = request_api_data(first_five)
+    return get_password_leaks_count(response, tail)
+
+
+@app.route('/pwned', methods=['POST', 'GET'])
+def main():
+    if request.method == 'POST':
+        password = request.form['password']
+        count = pwned_api_check(password)
+        return render_template('pwned.html', count=count, password=password)
+
+
+@app.route('/ImageProcessing', methods=['POST', 'GET'])
+def image_processing():
+    if request.method == 'POST':
+        f = request.files['image']
+        f.save('./static/assets/images/'+f.filename)
+        return render_template('ImageProcessing.html', file=f.filename)
+
